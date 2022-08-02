9d98c36fd6e9541b99d31108bb6a400ee985c4b9
V蛋 <610550052@qq.com>
2022-08-02 14:28:33 +0800

    feat(core) 1、基于 Nuxt + Express 搭建的静态页面

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    foo
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="#f6f6f6"
+      flat
+      light
+      outlined
+    >
+      <v-app-bar-title>
+        <span class="d-flex align-end">
+          <span style="height: 32px;width: 32px;line-height: 32px;margin-right: 2px;">
+            <img src="../static/icon.png">
+          </span>
+          ReadyRun
+        </span>
+      </v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container :class="$style.container" fluid>
+        <div :class="$style.page">
+          <h2>
+            Download
+          </h2>
+          <div :class="$style.image" />
+        </div>
+
+        <div class="d-flex my-4">
+          <v-btn
+            class="mx-auto"
+            color="#23272a"
+            dark
+            large
+            style="border-radius: 28px;"
+          >
+            <v-icon class="mr-2">
+              mdi-download
+            </v-icon>
+            Windows 版下载
+          </v-btn>
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {}
+</script>
+
+<style module scoped>
+.container {
+  max-width: 960px;
+  margin: 0;
+  padding: 0;
+  margin: 0 auto;
+}
+
+.page {
+  margin: 0 auto;
+  padding-top: 72px;
+}
+
+.image {
+  height: 320px;
+  background-image: url("../static/download-bg.jpg");
+  background-size: cover;
+  background-position: 50%;
+}
+</style>
